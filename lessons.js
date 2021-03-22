928c9b74573d2e650a090cbd4c16dfd648a7bc3b
Fatima Saeed <sareedfatima@gmail.com>
2021-03-22 22:34:19 +0400

    create lessons.js

---
+++ lessons.js
@@ -0,0 +1,110 @@
+let lessons = [
+    {
+        "id": 01,
+        "subject": 'IT',
+        "location": "Mauritius",
+        "price": 300,
+        "image": "https://www.intelligenthq.com/wp-content/uploads/2020/09/How-Tech-is-Changing-the-Way-we-Work.jpg",
+        "availableInventory": 5,
+        "rating": 3,
+    },
+    {
+        "id": 02,
+        "subject": 'Business Tech',
+        "location": "Dubai",
+        "price": 250,
+        "image": "https://industrywired.com/wp-content/uploads/2020/01/Business-Tech.jpg",
+        "availableInventory":5,
+        "rating": 4
+    },
+    {
+        "id": 03,
+        "subject": 'Law',
+        "location": "Hendon",
+        "price": 200,
+        "image": "https://cdn2.hubspot.net/hubfs/2509857/o2/img/blog/Blog%20Images%20%28migration%20blogs%29/Legal%20Corner%20UAE%20Competition%20Law%20%E2%80%93%20Now%20Effective.jpg",
+        "availableInventory":5,
+        "rating": 5
+    },
+    {
+        "id": 04,
+        "subject": 'Pharmacy',
+        "location": "Mauritius",
+        "price": 150,
+        "image": "https://www.rizmona.com/wp-content/uploads/2019/12/Pharmacy-business.jpg",
+        "availableInventory": 5,
+        "rating": 3,
+    },
+    {
+        "id": 05,
+        "subject": 'English',
+        "location": "Dubai",
+        "price": 200,
+        "image": "img/english.jpg",
+        "availableInventory":5,
+        "rating": 4
+    },
+    {
+        "id": 06,
+        "subject": 'Medicine',
+        "location": "Hendon",
+        "price": 350,
+        "image": "img/law.jpg",
+        "availableInventory":5,
+        "rating": 5
+    },
+    {
+        "id": 07,
+        "subject": 'History',
+        "location": "Mauritius",
+        "price": 170,
+        "image": "img/history.jpg",
+        "availableInventory": 5,
+        "rating": 3,
+    },
+    {
+        "id": 08,
+        "subject": 'Science',
+        "location": "Dubai",
+        "price": 240,
+        "image": "img/science.jpg",
+        "availableInventory":5,
+        "rating": 4
+    },
+    {
+        "id": 09,
+        "subject": 'Math',
+        "location": "Hendon",
+        "price": 190,
+        "image": "img/math.jpg",
+        "availableInventory":5,
+        "rating": 5
+    },
+    {
+        "id": 010,
+        "subject": 'Engineering',
+        "location": "Mauritius",
+        "price": 200,
+        "image": "img/demo-img.svg",
+        "availableInventory": 5,
+        "rating": 3,
+    },
+    {
+        "id": 011,
+        "subject": 'Robotics',
+        "location": "Dubai",
+        "price": 300,
+        "image": "img/demo-img.svg",
+        "availableInventory":5,
+        "rating": 4
+    },
+    {
+        "id": 012,
+        "subject": 'Software Engineering',
+        "location": "Hendon",
+        "price": 260,
+        "image": "img/demo-img.svg",
+        "availableInventory":5,
+        "rating": 5
+    }
+]
